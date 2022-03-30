5b7a12fee5c68a8c530fbdaec1e93244504dc614
Ashley Oyewole <ashleyoye24@gmail.com>
2022-03-30 03:44:37 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Link} from "react-router-dom";
+
+let nameInput = window.prompt("Enter your full name: ");
+
+if (nameInput == '') {
+  nameInput = 'stranger';
+}
+
+function formatName(user) {
+  return nameInput;
+}
+
+const user = {
+  name: nameInput
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= 'parent'>
+      <div className= 'div1'>
+
+        <div className="App">
+          <div className='floaters'>
+            <span>Settings</span>
+            <span>Log out</span>
+          </div>
+        
+          <h1>Hello, {formatName(user)}!</h1>
+
+          <h2>Your dashboard</h2>
+
+          <p>Welcome to your dashboard! Here you can find a summary of all your uploaded files and job offers.</p>
+
+          <button>Applications (1)</button>
+          <button>Offers (2)</button>
+          <button>Interviews (0)</button>
+          <button className='var-2'>Upload resume</button>  
+        </div>
+
+        <br></br>
+
+        <div className="App">
+          <h2>Job resources</h2>
+          <p>Check back soon!</p>
+        </div>
+
+        <br></br>
+      
+        <div className="App">
+          <h2>Month summary</h2>
+          <p>Nothing yet!</p>
+        </div>
+
+      </div>
+
+      <div className="div2">
+      
+        <div className='App'>
+          <h2>🔔 Notifications</h2>
+          <p>No new notifications.</p>
+
+        </div>
+      </div>
+
+      <div>
+        <span className='footer'>(c) 2022 Dasher</span>
+      </div>
+
     </div>
+    
+    
   );
 }
 
---
+++ src/components/files.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const fileUpload = () => {
+    return (
+      <div className='App'>
+        <h2>Hello</h2>
+      </div>
+    )
+  }
+  
+  export default fileUpload
